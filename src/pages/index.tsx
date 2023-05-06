ee923bae80e0ae67508a42ee085b20e3e8b8d797
kuluruvineeth <kuluruvineeth8623@gmail.com>
2023-05-06 12:40:57 +0530

    completed main section design layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,8 +31,8 @@ const HomePage = () => {
         </div>
       </header>
       <section className="grid h-full w-full grid-cols-12 place-items-center">
-        <main className="col-span-8 h-full w-full border-r border-gray-300">
-          <div className="flex w-full flex-col space-y-4  px-24 py-10">
+        <main className="col-span-8 h-full w-full border-r border-gray-300 px-24">
+          <div className="flex w-full flex-col space-y-4 py-10">
             <div className="flex w-full items-center space-x-4">
               <label
                 htmlFor="search"
@@ -50,12 +50,11 @@ const HomePage = () => {
                 />
               </label>
               <div className="flex w-full items-center justify-end space-x-4">
-                <div>My topics:</div>
                 <div className="flex items-center space-x-2">
                   {Array.from({ length: 2 }).map((_, i) => (
                     <div
                       key={i}
-                      className="rounded-3xl bg-gray-200/50 px-4 py-3"
+                      className="rounded-2xl bg-gray-200/50 px-6 py-2.5"
                     >
                       tag {i}
                     </div>
@@ -75,7 +74,59 @@ const HomePage = () => {
               </div>
             </div>
           </div>
-          <div></div>
+          <div className="flex w-full flex-col justify-center space-y-8">
+            {Array.from({ length: 5 }).map((_, i) => (
+              <div
+                key={i}
+                className="group flex flex-col space-y-8 border-b border-gray-300 pb-8 last:border-none"
+              >
+                <div className="flex w-full items-center space-x-2">
+                  <div className="h-10 w-10 rounded-full bg-gray-400"></div>
+                  <div>
+                    <p className="font-semibold">
+                      Kuluru Vineeth &#x2022; 05 May, 2023
+                    </p>
+                    <p className="text-sm">
+                      Developer, Husteler and Startup In Rising
+                    </p>
+                  </div>
+                </div>
+                <div className="grid w-full grid-cols-12 gap-4">
+                  <div className="col-span-8 flex flex-col space-y-4">
+                    <p className="text-2xl font-bold text-gray-800 decoration-indigo-600 group-hover:underline">
+                      Software Developer and Problem Solver,
+                    </p>
+                    <p className="break-words text-sm text-gray-500">
+                      Software Developer and Problem Solver, Initially I thought
+                      mastering one technology and becoming good at it would
+                      suffice our needs on daily basis. Till today with whatever
+                      exposure i had in the phase of my career i could say i was
+                      totally wrong about it. I strongly believe that having
+                      broader domain knowledge and technology-agnostic mindset
+                      is key to take us closer to the solution we are all
+                      desiring for.
+                    </p>
+                  </div>
+                  <div className="col-span-4">
+                    <div className="duration-105 h-full w-full transform rounded-xl bg-gray-500 transition hover:scale-105 hover:shadow-xl"></div>
+                  </div>
+                </div>
+                <div className="flex w-full items-center space-x-4">
+                  <div>My topics:</div>
+                  <div className="flex items-center space-x-2">
+                    {Array.from({ length: 2 }).map((_, i) => (
+                      <div
+                        key={i}
+                        className="rounded-2xl bg-gray-200/50 px-6 py-2.5"
+                      >
+                        tag {i}
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </main>
         <aside className="col-span-4 h-full w-full">This is sidebar</aside>
       </section>
